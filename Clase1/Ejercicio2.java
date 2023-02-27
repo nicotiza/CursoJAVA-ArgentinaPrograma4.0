8d137a9fb116d851f3fd555c9536298aef784417
nicotiza <nico.tiza@gmail.com>
2023-02-27 09:26:45 -0300

    Resolución de ejercicios Clase 1
    Aquí subo los ejercicios de la Clase 1 en 2 archivos:
    main.java (Ejercicio 1)
    Ejercicio2.java (Ejercicio 2)

---
+++ Clase1/Ejercicio2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        float ingresosTotales = 0.0f;
+        int cantVehiculos = 0;
+        int edadVehiculos = 0;
+        int inmuebles = 0;
+        boolean posesionesExtras = false;
+        Scanner KBinput = new Scanner(System.in);
+
+        System.out.println("Para saber su segmento de ingresos, favor de completar los siguientes datos: ");
+        System.out.print("Ingresos Totales: ");
+        ingresosTotales = KBinput.nextFloat();
+        System.out.print("Cantidad de Vehículos: ");
+        cantVehiculos = KBinput.nextInt();
+        System.out.print("Antigüedad del/los Vehículo/s: ");
+        edadVehiculos = KBinput.nextInt();
+        System.out.print("Cantidad de Inmuebles: ");
+        inmuebles = KBinput.nextInt();
+        System.out.println("¿Usted posee alguna embarcación, una aeronave de lujo o \nser titular de activos societarios que demuestren capacidad económica plena?\nIndique true ó false");
+        posesionesExtras = KBinput.nextBoolean();
+
+        if (ingresosTotales>=489083 || cantVehiculos>=3 && edadVehiculos<=5 || inmuebles>=3 || posesionesExtras) {
+            System.out.println("Usted pertenece al segmento de ingresos Altos.");
+        } else {
+            System.out.println("Usted no pertenece al segmento de ingresos Altos.\nÉsta fue una versión de prueba, para conocer su segmento correspondiente favor de adquirar la versión completa.\nCerrando sistema...");
+        }
+    }
+}
---
+++ Clase1/Main.java
@@ -0,0 +1,155 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //
+        //
+        //
+        /*------------------------ Creación de Variables, Scanners, etc. ------------------------*/
+        int primerNum = 0;
+        int segundoNum = 0;
+        boolean deseaParImpar = false;
+        String condicionBusqueda = "";
+        Scanner KBinput = new Scanner(System.in);
+        /*------------------------ Creación de Variables, Scanners, etc. ------------------------*/
+        //
+        //
+        //
+        /*------------------------ El cliente ingresa los datos ------------------------*/
+        System.out.println("Ingrese el primer número: ");
+        primerNum = KBinput.nextInt();
+        System.out.println("Ingrese el segundo número: ");
+        segundoNum = KBinput.nextInt();
+        /*------------------------ El cliente ingresa los datos ------------------------*/
+        //
+        //
+        //
+        /*------------------------ Se informa los números ingresados ------------------------*/
+        imprimirNumeros(primerNum,segundoNum);
+        /*------------------------ Se informa los números ingresados ------------------------*/
+        //
+        //
+        //
+        /*------------------------ Salto de Línea ------------------------*/
+                                System.out.println("\n\n");
+        /*------------------------ Salto de Línea ------------------------*/
+        //
+        //
+        //
+        /*------------------------ Se consulta si desea buscar Pares ó Impares. ------------------------*/
+        System.out.println("¿Desea buscar Pares ó Impares? (true/false)");
+        deseaParImpar = KBinput.nextBoolean();
+
+        if (deseaParImpar) {
+            System.out.println("¿Qué desea buscar? ¿Pares ó Impares?.");
+            condicionBusqueda = KBinput.next();
+
+
+            switch (condicionBusqueda) {
+                case "pares":
+                    System.out.println("Excelente! Enseguida te indico los números pares del rango ingresado...");
+                    busquedaParesImpares("pares",primerNum,segundoNum);
+                    break;
+                case "impares":
+                    System.out.println("Excelente! Enseguida te indico los números impares del rango ingresado...");
+                    busquedaParesImpares("impares",primerNum,segundoNum);
+                    break;
+                default:
+                    System.out.println("¿¿¿No desea nada entonces??? \n ¡Gracias! ¡Vuelvas prontos!! XD");
+            }
+
+        } else {
+            System.out.println("Muchas gracias por participar de éste experimento. Saludos terrícola!");
+        }
+    }
+    //
+    //
+    //
+    /*--------------------------    FUNCIONES   -----------------------------------------*/
+    //
+    //
+    //
+    /*----------    Imprime en Pantalla el rango de los números ingresados   ------------*/
+    private static void imprimirNumeros (int num1, int num2){
+        System.out.println("El rango de los números ingresados es: ... ");
+        if (num1 <= num2) {
+            System.out.println("El primer número es inferior o igual al segundo. Ordenando de menor a mayor...");
+            while (num1 <= num2) {
+                System.out.print(num1 + " ");
+                num1 ++;
+            }
+        } else {
+            System.out.println("El segundo número es menor al primero. Ordenando de mayor a menor...");
+            while (num1 >= num2) {
+                System.out.print(num1 + " ");
+                num1 --;
+            }
+        }
+    }
+    /*----------    Imprime en Pantalla el rango de los números ingresados   ------------*/
+    //
+    //
+    //
+    /*---- Busca los números Pares ó Impares según se halla llamado a la función.   -----*/
+
+    private static void busquedaParesImpares(String condicion, int num1, int num2){
+
+
+        if (condicion == "pares") {
+            System.out.println("Los números pares son: ");
+
+            if (num1 <= num2) {
+                System.out.println("Ordenando de menor a mayor...");
+                while (num1 <= num2) {
+                    if (num1 % 2 == 0) {
+                        System.out.print(num1 + " ");
+                    }
+                    num1 ++;
+                }
+                /*
+                System.out.println("\n Ahora en orden invertido con sentencia FOR:");
+                for (int i = num2; i>=num1; i--){
+                    System.out.println(i + " ");
+                }*/
+            } else {
+                System.out.println("Ordenando de mayor a menor...");
+                while (num1 >= num2) {
+                    if (num1 % 2 == 0) {
+                        System.out.print(num1 + " ");
+                    }
+                    num1 --;
+                }
+                /*
+                System.out.println("\n Ahora en orden invertido con sentencia FOR:");
+                for (int i=num1; i>num2; i--){
+                    System.out.println(i + " ");
+                }*/
+            }
+
+
+        } else {
+            if (condicion == "impares"){
+                System.out.println("Los números impares son: ");
+
+                if (num1 <= num2) {
+                    System.out.println("Ordenando de menor a mayor...");
+                    while (num1 <= num2) {
+                        if (num1 % 2 != 0) {
+                            System.out.print(num1 + " ");
+                        }
+                        num1 ++;
+                    }
+                } else {
+                    System.out.println("Ordenando de mayor a menor...");
+                    while (num1 >= num2) {
+                        if (num1 % 2 != 0) {
+                            System.out.print(num1 + " ");
+                        }
+                        num1 --;
+                    }
+                }
+            }
+        }
+    }
+
+}
